92e27144231d871b6387a8a7dbac8257f3df9583
Amy Welch <awelch@thoughtworks.com>
2019-12-04 11:40:42 +0000

    [PRMT-698] use environment to determine whether to connect to MHS

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,7 @@ const config = {
   pdsAsid: process.env.PDS_ASID,
   deductionsAsid: process.env.DEDUCTIONS_ASID,
   mhsUrl: process.env.MHS_URL,
-  // TODO: use NODE_ENV environment variable when MHS networking is finished
-  isLocal: true // process.env.NODE_ENV === 'local'
+  isLocal: process.env.NODE_ENV === 'local'
 };
 
 export default config;
