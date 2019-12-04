528f2b8d33757042e849b5c95a9d0b0f8175c730
Amy Welch <awelch@thoughtworks.com>
2019-12-04 09:52:52 +0000

    [PRMT-698] set isLocal config item to true until MHS networking is finished

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ const config = {
   pdsAsid: process.env.PDS_ASID,
   deductionsAsid: process.env.DEDUCTIONS_ASID,
   mhsUrl: process.env.MHS_URL,
-  isLocal: process.env.NODE_ENV === 'local'
+  // TODO: use NODE_ENV environment variable when MHS networking is finished
+  isLocal: true // process.env.NODE_ENV === 'local'
 };
 
 export default config;
